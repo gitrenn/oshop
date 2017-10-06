3247bb5b110116afc3e31536a5fa597ebe6debf3
Ren <rjingsiu@gmail.com>
2017-10-05 23:12:18 -0700

    added dropdown item names and fixed routes issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,7 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
       { path: '', component: HomeComponent },
       { path: 'products', component: ProductsComponent },
       { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: 'my-orders', component: MyOrdersComponent},
       { path: 'order-success', component: OrderSuccessComponent },
       { path: 'login', component: LoginComponent },
       { path: 'admin/products', component: AdminProductsComponent },
