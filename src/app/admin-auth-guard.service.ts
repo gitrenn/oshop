0d647d83223f395f747e9133f23b33dab4537c09
Ren <rjingsiu@gmail.com>
2017-10-08 22:27:02 -0700

    hided admin links from unauthroized access

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -3,17 +3,17 @@ import { UserService } from './user.service';
 import { AuthService } from './auth.service';
 import { CanActivate } from '@angular/router';
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/switchMap';
+
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AdminAuthGuard implements CanActivate{
+  
 
   constructor(private auth: AuthService, private userService: UserService) { }
 
   canActivate(): Observable<boolean>{
-    return this.auth.user$
-      .switchMap(user => this.userService.get(user.uid))
+    return this.auth.appUser$
       .map(appUser => appUser.isAdmin);
   }
 }
