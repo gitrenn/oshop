8fb0d81ceb79fa14ba3d3fb6571d3a6075cdf21a
Ren <rjingsiu@gmail.com>
2017-10-08 21:48:10 -0700

    protected admin routes from unauthorized access

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AdminAuthGuard } from './admin-auth-guard.service';
 import { UserService } from './user.service';
 import { AuthGuard } from './auth-guard.service';
 import { AuthService } from './auth.service';
@@ -52,14 +53,23 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
       { path: 'my-orders', component: MyOrdersComponent},
       { path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard] },
       
-      { path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard] },
-      { path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard] },
+      { 
+        path: 'admin/products', 
+        component: AdminProductsComponent, 
+        canActivate: [AuthGuard, AdminAuthGuard] 
+      },
+      { 
+        path: 'admin/orders', 
+        component: AdminOrdersComponent, 
+        canActivate: [AuthGuard, AdminAuthGuard] 
+      },
     ])
   ],
   providers: [
     AuthService,
     AuthGuard,
-    UserService
+    UserService,
+    AdminAuthGuard
   ],
   bootstrap: [AppComponent]
 })
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,4 +1,5 @@
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AppUser } from './models/app-user';
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 
@@ -8,10 +9,13 @@ export class UserService {
   constructor(private db: AngularFireDatabase) { }
 
   save(user: firebase.User) {
-    this.db.object('/user/' + user.uid).update({
+    this.db.object('/users/' + user.uid).update({
       name: user.displayName,
       email: user.email
     })
   }
 
+  get(uid: string): FirebaseObjectObservable<AppUser>{
+    return this.db.object('/users/' + uid);
+  }
 }
