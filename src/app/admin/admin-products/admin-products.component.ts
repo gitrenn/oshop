2067c969cc300c4f6f570dd9980ba575d1cfcd4b
Ren <rjingsiu@gmail.com>
2017-10-11 11:49:16 -0700

    added search functions to searh items in the list

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,33 @@
+import { Subscription } from 'rxjs/Rx';
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products:{title: string}[];//in the future, an interface of products should be created (product[])
+  filteredProducts: any[]; // this array will be displayed in the template
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
-   }
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
+  }
+
+  filter(query: string){
+    //console.log(query);
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : 
+      this.products;
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
