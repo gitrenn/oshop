88b8ddf75ee9892879acb8d87a065516c845bd49
Ren <rjingsiu@gmail.com>
2017-10-12 20:31:00 -0700

    created functions to make change buttons work

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -14,13 +14,17 @@ export class ProductCardComponent {
 
   constructor(private shoppingCartService: ShoppingCartService) { }
 
-  addToCart(product: Product){
-    this.shoppingCartService.addToCart(product);
+  addToCart(){
+    this.shoppingCartService.addToCart(this.product);
+  }
+
+  removeFromCart(){
+    this.shoppingCartService.removeFromCart(this.product);
   }
 
   getQuantity(){
     if(!this.shoppingCart) return 0;
-    
+
     let item = this.shoppingCart.items[this.product.$key];
     return item ? item.quantity : 0;
   }
