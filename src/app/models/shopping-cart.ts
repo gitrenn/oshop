6fed698cbe6bdb4bec3a80a2f383eeebfdf0b9eb
Ren <rjingsiu@gmail.com>
2017-10-13 10:46:27 -0700

    extracted product quantity component to make add and minus buttons reusable

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,3 +1,4 @@
+import { Product } from './product';
 import { ShoppingCartItem } from './shopping-cart-item';
 
 export class ShoppingCart {
@@ -7,9 +8,13 @@ export class ShoppingCart {
         for(let productId in itemsMap){
             let item = itemsMap[productId];
             this.items.push(new ShoppingCartItem(item.product, item.quantity));
-        }
-            
+        }    
     }
+
+    getQuantity(product: Product){
+        let item = this.itemsMap[product.$key];
+        return item ? item.quantity : 0;
+      }
 
     get totalPrice(){
         let sum = 0;
--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,3 +1,4 @@
+import { ShoppingCart } from './../models/shopping-cart';
 import { ShoppingCartService } from '../shopping-cart.service';
 import { Product } from './../models/product';
 import { Component, OnInit, Input } from '@angular/core';
@@ -10,22 +11,11 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProductCardComponent {
   @Input('product') product: Product;
   @Input('show-actions') showActions = true;
-  @Input('shopping-cart') shoppingCart;
+  @Input('shopping-cart') shoppingCart: ShoppingCart;
 
   constructor(private shoppingCartService: ShoppingCartService) { }
 
   addToCart(){
     this.shoppingCartService.addToCart(this.product);
-  }
-
-  removeFromCart(){
-    this.shoppingCartService.removeFromCart(this.product);
-  }
-
-  getQuantity(){
-    if(!this.shoppingCart) return 0;
-
-    let item = this.shoppingCart.items[this.product.$key];
-    return item ? item.quantity : 0;
   }
 }
